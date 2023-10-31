5a029467320c7328434aeb31913d8a8a6ed9200a
Vothuong <nguyenkieuvuong@gmail.com>
2023-10-31 22:45:59 +0700

    mvc

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,13 @@
 import express from "express";
+import homeController from "../controller/homeController";
 const router = express.Router();
 const initWebRoutes = (app) =>{
-    router.get("/", (req,res)=>{
-        return res.send("hello word");
-    })
+    // router.get("/", (req,res)=>{
+    //     return res.send("hello word");
+    // })
+    // path, handler
+    router.get("/",homeController.handleHelloWord);
+    router.get("/user",homeController.handleUserPage);
     return app.use("/",router);
 }
 export default initWebRoutes;
